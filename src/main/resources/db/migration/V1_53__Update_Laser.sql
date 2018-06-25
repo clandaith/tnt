bbab118668e5731f788c0b3a9e9205dd0ecc7905
Troy Davidson <tdavidson@pointandpay.com>
2018-06-25 12:34:57 -0600

    Updated Laser text

---
+++ src/main/resources/db/migration/V1_53__Update_Laser.sql
@@ -0,0 +1,3 @@
+
+
+update special_rules set description = 'Laser weapons allow the shooter to roll 2D10 and choose the highest result when making Ranged attacks. However, the weapon will Malfunction whenever any double result is rolled, not just on 1’s.' where name = 'Laser';
